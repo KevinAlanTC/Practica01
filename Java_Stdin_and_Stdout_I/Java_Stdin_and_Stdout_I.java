bae3b155cef79a617953f15011bc74f9202f87bb
KATC <323160698@comunidad.unam.mx>
2025-12-14 23:01:55 -0600

    Create Java_Stdin_and_Stdout_I.java

---
+++ Java_Stdin_and_Stdout_I/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int num3 = scan.nextInt();
+        scan.close();
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
